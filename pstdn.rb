88d7046dd2dcd99ea942f29821dd22a526366b1c
huequica <harumaki1225@outlook.com>
2018-08-16 16:38:30 +0900

    Bug fixed(please readme.md for details)

--- pstdn.rb
+++ pstdn.rb
@@ -11,16 +11,22 @@ puts "** クライアントを終了させたい場合は 'exit' と入力して
 loop{    
     print "text>>"
     input_Text = readlines
-    
-    if input_Text[0] == "exit" || input_Text[0] =="exit\n"  then 
-        break
-    end
 
-    if input_Text.join =~ /^.*!Clear.*\n/ || input_Text.join =~ /^.*!clear.*\n/         #入力文字列の中に!Clear(大文字小文字判別しない)が含まれた場合
-                                                                                        #トゥートごとキャンセルしてコマンドラインもきれいにする
-        system ('clear')
-    else
-        toot = connection.create_status(input_Text.join)
-        print "** Tooted. **\n" + "#{toot.content}" + "\n"
+    if input_Text.join == "" then
+        puts "\n** なにも入力されていません。文字列を入力してからCtrl+Dを叩いてください。 **\n"
+    else 
+        #ここからなにか入ってた場合の処理
+
+        if input_Text[0] == "exit" || input_Text[0] =="exit\n"
+            break
+        end
+    
+        if input_Text.join =~ /^.*!Clear.*\n/ || input_Text.join =~ /^.*!clear.*\n/         #入力文字列の中に!Clear(大文字小文字判別しない)が含まれた場合
+            system ('clear')                                                                #トゥートごとキャンセルしてコマンドラインもきれいにする
+        else
+            toot = connection.create_status(input_Text.join)
+            print "** Tooted. **\n" + "#{toot.content}" + "\n"
+        end
+    
     end
 }
