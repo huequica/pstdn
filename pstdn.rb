342bf6b32b441879caa0c422285a9b289aafd28f
huequica <harumaki1225@outlook.com>
2018-08-22 11:52:41 +0900

    Implement CW(please check readme.md)

--- pstdn.rb
+++ pstdn.rb
@@ -1,18 +1,19 @@
-require 'mastodon'
-
 MASTODON_HOST = 'https://mstdn.jp'      #この中身変えればインスタンス変更できます。サポートはしませんがご自由にどうぞ
 
-connection = Mastodon::REST::Client.new(base_url: MASTODON_HOST, bearer_token: ENV['MASTODON_ACCESS_TOKEN'])
-
 system ('clear')                        #起動時にコマンドラインキレイキレイに
-puts "** 接続に成功しました: #{connection.base_url} **" 
-puts "** クライアントを終了させたい場合は 'exit' と入力して改行したのち、 Ctrl+D を叩いて下さい **"
+puts "** 終了させたい場合は 'exit' と入力して改行したのち、 Ctrl+D を叩いて下さい **"
+
+#curl -X POST -d "status=膣&spoiler_text=test" --header "Authorization: Bearer $MASTODON_ACCESS_TOKEN" -sS https://mstdn.jp/api/v1/statuses;
+curl_base = "curl -o /dev/null -w '%{http_code}\n' -X POST --header \"Authorization: Bearer \"" + ENV['MASTODON_ACCESS_TOKEN'] + " -s https://mstdn.jp/api/v1/statuses "
 
 
 loop{    
     print "text>>"
     input_Text = readlines
     $CW_flag = nil
+    curl_status = nil
+    curl_cmd = nil
+
     if input_Text.join == "" then
         puts "\n** なにも入力されていません。文字列を入力してからCtrl+Dを叩いてください。 **\n"
     else 
@@ -30,24 +31,24 @@ loop{
         else
             $CW_flag = false
         end
-    
+
+        
         if input_Text.join =~ /^.*!Clear.*\n/ || input_Text.join =~ /^.*!clear.*\n/         #入力文字列の中に!Clear(大文字小文字判別しない)が含まれた場合
             system ('clear')                                                                #トゥートごとキャンセルしてコマンドラインもきれいにする
         else
             #トゥート処理、ただしCWの有無で分岐する
-            if $CW_flag != true then
-                toot = connection.create_status(input_Text.join)
-                print "** Tooted. **\n" + "#{toot.content}" + "\n"
-                
-            else
-                toot = connection.create_status(
-                    input_Text.join, 
- 
-                    spoiler_text = CW_text)
+            curl_status = "-d \"status=" + input_Text.join
 
-                    
-                    pp toot
-                #print "** Tooted. **\n" + "#{toot.content}" + "\n"
+            if $CW_flag != true then
+                curl_status += "\""
+                curl_cmd = curl_base + curl_status
+                
+                toot = system(curl_cmd)
+            else
+                curl_status += "&spoiler_text=" + CW_text + "\""
+                curl_cmd = curl_base + curl_status
+
+                toot = system(curl_cmd)
 
             end    
         end
