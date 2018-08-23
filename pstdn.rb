cc5ddb570312cb0feb777ca11d5ff025b4479d12
huequica <harumaki1225@gmail.com>
2018-08-24 01:56:06 +0900

    APIの叩き方を変更、その他(please check readme.md)

--- pstdn.rb
+++ pstdn.rb
@@ -1,18 +1,27 @@
+require 'net/http'
+require 'uri'
+
 MASTODON_HOST = 'https://mstdn.jp'      #この中身変えればインスタンス変更できます。サポートはしませんがご自由にどうぞ
 
 system ('clear')                        #起動時にコマンドラインキレイキレイに
 puts "** 終了させたい場合は 'exit' と入力して改行したのち、 Ctrl+D を叩いて下さい **"
 
-#curl -X POST -d "status=膣&spoiler_text=test" --header "Authorization: Bearer $MASTODON_ACCESS_TOKEN" -sS https://mstdn.jp/api/v1/statuses;
-curl_base = "curl -o /dev/null -w '%{http_code}\n' -X POST --header \"Authorization: Bearer \"" + ENV['MASTODON_ACCESS_TOKEN'] + " -s https://mstdn.jp/api/v1/statuses "
+#インスタンスアクセス前の処理
+access_token = ENV['MASTODON_ACCESS_TOKEN']
+uri = URI.parse('https://mstdn.jp')
+http = Net::HTTP::new(uri.host, uri.port)
+
+http.use_ssl = true
+
+request = Net::HTTP::Post.new('/api/v1/statuses')
+request.add_field('Authorization', "Bearer #{access_token}")
+
 
 
 loop{    
     print "text>>"
     input_Text = readlines
     $CW_flag = nil
-    curl_status = nil
-    curl_cmd = nil
 
     if input_Text.join == "" then
         puts "\n** なにも入力されていません。文字列を入力してからCtrl+Dを叩いてください。 **\n"
@@ -36,21 +45,21 @@ loop{
         if input_Text.join =~ /^.*!Clear.*\n/ || input_Text.join =~ /^.*!clear.*\n/         #入力文字列の中に!Clear(大文字小文字判別しない)が含まれた場合
             system ('clear')                                                                #トゥートごとキャンセルしてコマンドラインもきれいにする
         else
-            #トゥート処理、ただしCWの有無で分岐する
-            curl_status = "-d \"status=" + input_Text.join
+            toot_body = 'status=' + input_Text.join
+            #CWの有無で分岐する
+            if $CW_flag == true then
+                toot_body += '&spoiler_text=' + CW_text
+            end
 
-            if $CW_flag != true then
-                curl_status += "\""
-                curl_cmd = curl_base + curl_status
-                
-                toot = system(curl_cmd)
+            request.body = toot_body
+            response = http.request(request)
+            
+            #トゥート成功か失敗か確認する
+            if response.code == '200'
+                puts '** Toot Success. **'
             else
-                curl_status += "&spoiler_text=" + CW_text + "\""
-                curl_cmd = curl_base + curl_status
-
-                toot = system(curl_cmd)
-
-            end    
+                puts '** エラー起きたかも **'
+            end
         end
     
     end
