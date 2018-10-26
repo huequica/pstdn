49932db38207160e15d5b6991f0470118d0c66b1
huequica <harumaki1225@gmail.com>
2018-10-26 13:23:21 +0900

    add another accounts toot(please check README.md)

---
+++ param.rb
@@ -0,0 +1,7 @@
+#   name  : インスタンス名
+#   token : インスタンスで取得したアクセストークン
+
+$auth_data = [
+    {"name" => "mstdn.jp", "token" => "f98fff51b9ebaaa465127455f15c692ab1c937ca2d9bafcc07851f6621a35569", "default" => true},
+    {"name" => "knzk.me", "token" => "7afb4c3cf7c602daa84bbed491240701e4512123ed5be7f7fca24161c8911cbb", "default" => false }
+]
--- pstdn.rb
+++ pstdn.rb
@@ -1,112 +1,168 @@
 require 'net/http'
 require 'uri'
+require './param'
 
 #################################################################################
 #########################      定義メソッド置き場      #############################
 #################################################################################
 def visiv_select()
-    
-    puts '0 public'
-    puts '1 private'
-    puts '2 unlisted'
-    puts '3 direct(recomennd to use Reply only)'
-    puts '4 Canncel'
-    print '番号を入力してください >>'
-    visiv_param = ['public', 'private', 'unlisted', 'direct', false]
+  puts '0 public'
+  puts '1 private'
+  puts '2 unlisted'
+  puts '3 direct(recomennd to use Reply only)'
+  puts '4 Canncel'
+  print '番号を入力してください >>'
+  visiv_param = ['public', 'private', 'unlisted', 'direct', false]
 
-    loop do 
-        number = readline.to_i
-        if number.between?(0, 4) then
-            return visiv_param[number]
-            break
-        else
-            print '正しい値を入れてください>>'
-        end
+  loop do
+    number = readline.to_i
+    if number.between?(0, 4) then
+      return visiv_param[number]
+      break
+    else
+      print '正しい値を入れてください>>'
     end
-    
-
+  end
 end
 
+def change_inst()
 
+  $auth_data.each do |data|
+    puts "#{$auth_data.index(data)} : #{data["name"]}"
+  end
+
+  puts "#{$auth_data.size} : キャンセルして戻る"
+  puts '** 番号を選んでください **'
+  print '>>'
+  input_val = readline.to_i
+
+  if input_val == $auth_data.size
+    input_val = false
+  end
+
+  return input_val
+end
 
 #################################################################################
 #########################      main Method      #################################
+#起動時インスタンスの選択
+running_flag = false
+count = 0
 
-MASTODON_HOST = 'https://mstdn.jp'      #この中身変えればインスタンス変更できます。サポートはしませんがご自由にどうぞ
+while count <= $auth_data.size - 1
+  if $auth_data[count]["default"] == true then
+    system ('clear')#起動時にコマンドラインキレイキレイに
+    running_flag = true
 
-system ('clear')                        #起動時にコマンドラインキレイキレイに
+
+    mastodon_host = "https://#{$auth_data[count]["name"]}"
+    access_token = $auth_data[count]["token"]
+
+    uri = URI.parse(mastodon_host)
+    http = Net::HTTP.new(uri.host, uri.port)
+
+    http.use_ssl = true
+
+    request = Net::HTTP::Post.new('/api/v1/statuses')
+    request.add_field('Authorization', "Bearer #{access_token}")
+    puts "** #{$auth_data[count]["name"]}が選択されています **"
+    break
+  else
+    count += 1
+  end
+end
+
+#どれにもdefaultの中にtrueが入ってなかった場合
+
+if running_flag != true
+  puts 'デフォルト指定がされていません。param.rbをいじってください'
+  exit
+end
+
+
 puts "** 終了させたい場合は 'exit' と入力して改行したのち、 Ctrl+D を叩いて下さい **"
 
-#インスタンスアクセス前の処理
-access_token = ENV['MASTODON_ACCESS_TOKEN']
-uri = URI.parse('https://mstdn.jp')
-http = Net::HTTP::new(uri.host, uri.port)
 
-http.use_ssl = true
+loop do
+  print "text>>"
+  input_Text = readlines
 
-request = Net::HTTP::Post.new('/api/v1/statuses')
-request.add_field('Authorization', "Bearer #{access_token}")
+  if input_Text.join == "" then
+    puts "\n** なにも入力されていません。文字列を入力してからCtrl+Dを叩いてください。 **\n"
+  else
+    # ここからなにか入ってた場合の処理
 
+    if input_Text[0] == "set_inst\n" then #インスタンス変更処理
+      inst_val = change_inst()
+      if inst_val != false
+        system ('clear')#起動時にコマンドラインキレイキレイに
 
+        mastodon_host = "https://#{$auth_data[inst_val]["name"]}"
+        access_token = $auth_data[inst_val]["token"]
 
-loop{    
-    print "text>>"
-    input_Text = readlines
+        uri = URI.parse(mastodon_host)
+        http = Net::HTTP.new(uri.host, uri.port)
 
-    if input_Text.join == "" then
-        puts "\n** なにも入力されていません。文字列を入力してからCtrl+Dを叩いてください。 **\n"
-    else 
-        #ここからなにか入ってた場合の処理
+        http.use_ssl = true
 
-        if input_Text[0] == "exit" || input_Text[0] =="exit\n"
-            break
-        end
+        request = Net::HTTP::Post.new('/api/v1/statuses')
+        request.add_field('Authorization', "Bearer #{access_token}")
+        puts "** #{$auth_data[inst_val]["name"]}が選択されています **"
 
-        if input_Text[0] =~ /^.*!CW .*\n/
-
-            cw_flag = true
-            input_Text[0].slice!(/\A!CW /)
-            cw_text = input_Text[0]
-            input_Text.delete_at(0)
-            
-        else
-            cw_flag = false
-        end
-            
-        if input_Text.index{|item| item =~ /^!visiv/i} then
-            visiv_row = input_Text.index{|item| item =~ /^!visiv/i}
-            input_Text.delete_at(visiv_row)
-            visiv_flag = true
-        end
-
-        if input_Text.join =~ /^.*!Clear.*\n/i       #入力文字列の中に!Clear(大文字小文字判別しない)が含まれた場合
-            system ('clear')                         #トゥートごとキャンセルしてコマンドラインもきれいにする
-        else
-            toot_body = 'status=' + input_Text.join
-            #CWの有無で分岐する
-            if cw_flag == true then
-                toot_body += '&spoiler_text=' + cw_text
-            end
-
-            if visiv_flag then
-                visiv_set = visiv_select()
-                if visiv_set == false then
-                    next
-                end
-                toot_body += '&visibility=' + visiv_select
-            end
-
-
-            request.body = toot_body
-            response = http.request(request)
-            
-            #トゥート成功か失敗か確認する
-            if response.code == '200'
-                puts '** Toot Success. **'
-            else
-                puts '** エラー起きたかも **'
-            end
-        end
-    
+      end
+    next
     end
-}
+
+    if input_Text[0] == "exit" || input_Text[0] == "exit\n"
+      break
+    end
+
+    if input_Text[0] =~ /^.*!CW .*\n/
+
+      cw_flag = true
+      input_Text[0].slice!(/\A!CW /)
+      cw_text = input_Text[0]
+      input_Text.delete_at(0)
+
+    else
+      cw_flag = false
+    end
+
+    if input_Text.index { |item| item =~ /^!visiv/i } then
+      visiv_row = input_Text.index { |item| item =~ /^!visiv/i }
+      input_Text.delete_at(visiv_row)
+      visiv_flag = true
+    end
+
+    if input_Text.join =~ /^.*!Clear.*\n/i # 入力文字列の中に!Clear(大文字小文字判別しない)が含まれた場合
+      system ('clear') # トゥートごとキャンセルしてコマンドラインもきれいにする
+    else
+      toot_body = 'status=' + input_Text.join
+      # CWの有無で分岐する
+      if cw_flag == true then
+        toot_body += '&spoiler_text=' + cw_text
+      end
+
+      if visiv_flag then
+        visiv_set = visiv_select()
+        if visiv_set == false then
+          next
+        end
+
+        toot_body += '&visibility=' + visiv_select
+      end
+
+      request.body = toot_body
+      response = http.request(request)
+
+      # トゥート成功か失敗か確認する
+      if response.code == '200'
+        puts '** Toot Success. **'
+      else
+        puts '** エラー起きたかも **'
+      end
+
+    end
+
+  end
+end
